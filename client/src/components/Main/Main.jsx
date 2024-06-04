23c96b2d6dc11790feb78c70f8b701a54880d2b2
Dona4449 <murmu2000dona@gmail.com>
2024-06-05 01:56:57 +0530

    added main.css

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Main.css'
 
 function Main() {
   return (
     <>
         <h1 className="head">Phishing URL Detection</h1>
+  
+        <div className='url'>
+          <form action="/">
+            <span>
+              <input className="box" type="text" id="fname" placeholder='Paste URLs here' />
+            </span>
+            <span>
+              <input type="submit" value="Check" />
+            </span>
+          </form>
+        </div>
     </>
   )
 }
--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ayush\Desktop\Ardent\Phishing URL Project\server\venv
+set VIRTUAL_ENV=C:\Users\DONA\Desktop\ardent\Phishing-URL-Project\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
